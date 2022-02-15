032b104d3444f98cb87056309a3ed4c5d3914b57
zhenyolka <zhenyolka@gmail.com>
2022-02-15 15:48:15 +0300

    pyxis: Typo

--- device.mk
+++ device.mk
@@ -71,7 +71,7 @@ PRODUCT_PACKAGES += \
 		android.hardware.biometrics.fingerprint@2.3.vendor
 
 PRODUCT_COPY_FILES += \
-		frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml		
+		frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # FM
 BOARD_HAVE_QCOM_FM := true
